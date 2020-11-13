24646726efff1131d202896609f224fd39552ec8
vistritpandey <vistritpandey@gmail.com>
2020-11-13 08:35:14 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-
+import {MenuItem, FormControl, Select} from "@material-ui/core"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-            Hello there
-        </h1>
-        
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value="abc">
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+          </Select>
+        </FormControl>
       </header>
     </div>
   );
