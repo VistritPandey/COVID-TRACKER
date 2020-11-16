299d85b339a2b63526ba6e8761fb5fa2a0796c05
vistritpandey <vistritpandey@gmail.com>
2020-11-16 22:57:00 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import {
-  MenuItem,
-  FormControl,
-  Select,
-  Card,
-  CardContent,
-} from "@material-ui/core";
+import {MenuItem, FormControl, Select, Card, CardContent} from "@material-ui/core";
 import InfoBox from "./InfoBox";
 import LineGraph from "./LineGraph";
 import Table from "./Table";
