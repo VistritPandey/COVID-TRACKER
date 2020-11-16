840506e73d972100df8c0df631074de563a910af
vistritpandey <vistritpandey@gmail.com>
2020-11-16 22:09:54 +0530

    k

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,15 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Line } from "react-chartjs-2"
 
 function LineGraph() {
     const [data, setData] =useState({});
-// 
+// https://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+    .then(response => response.json())
+    .then(data => {
+        
+    })
+
+},[])
+
+const buildChartData = (data, casesType='cases') => {
+    const chartData = [];
+    let lastDataPoint;
+
+    data[casesType].forEach(date => {
+
+        if (lastDataPoint){
+            const newDataPoint ={
+                x: date,
+                y: data[casesType][date] - lastDataPoint
+            }
+            chartData.push(newDataPoint)
+        }
+        lastDataPoint = data[casesType][date];
+    })
+    return chartData;
+}
+
     return (
         <div className='linegraph'>
-            <Line 
+        <h1>I am the graph</h1>
+            {/*<Line 
                 data
                 options
-            />
+            />*/}
         </div>
     )
 }
