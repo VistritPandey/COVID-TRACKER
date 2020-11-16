15573eae42ee5f64f47dfd3c994f6132b2480e6b
vistritpandey <vistritpandey@gmail.com>
2020-11-16 22:44:49 +0530

    table completed

--- src/Table.js
+++ src/Table.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Table.css'
+import numeral from "numeral"
 
 function Table({countries}) {
     return (
         <div className='table'>
-            {countries.map(({country, cases}) => (
+            {countries.map((country) => (
                 <tr>
-                    <td>{country}</td>
+                    <td>{country.country}</td>
                     <td>
-                    <strong>{cases}</strong>
+                    <strong>{numeral(country.cases).format("0,0")}</strong>
                     </td>
                 </tr>
             ))}
